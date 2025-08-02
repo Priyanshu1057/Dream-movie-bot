a9e5e303c5d150246069fbc338a2b130a72cff48
priyanshuk978 <priyanshukumawat2k25@gmail.com>
2025-08-02 14:52:06 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -100,11 +100,16 @@ async def start(client, message):
         buttons = [[
                     InlineKeyboardButton('🔰 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🔰', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton(' ʜᴇʟᴘ 📢', callback_data='help'),
-                    InlineKeyboardButton(' ᴀʙᴏᴜᴛ 📖', callback_data='about')
+                    InlineKeyboardButton('「 ✦ ᴏᴡɴᴇʀ ✦ 」', url='http://t.me/MovixNest_ownerbot')
                 ],[
-                    InlineKeyboardButton('ᴛᴏᴘ sᴇᴀʀᴄʜɪɴɢ ⭐', callback_data="topsearch"),
-                    InlineKeyboardButton('ᴜᴘɢʀᴀᴅᴇ 🎟', callback_data="premium_info"),
+                    InlineKeyboardButton('✎ᝰ.ʜᴇʟᴘ', callback_data='help'),
+                    InlineKeyboardButton('ⓘ ᴀʙᴏᴜᴛ', callback_data='about')
+                ],[
+                   InlineKeyboardButton('〄 ᴄʜᴀɴɴᴇʟ 〄', url='http://t.me/MovixNest_ownerbot'),
+                   InlineKeyboardButton('〄 ɢʀᴏᴜᴘ 〄', url='https://t.me/MovixNest')
+                ],[
+                   InlineKeyboardButton('✦ ᴛʀᴇɴᴅɪɴɢ ✦', callback_data="topsearch"),
+                    InlineKeyboardButton('✦ ᴘʀᴇᴍɪᴜᴍ ✦', callback_data="premium_info")
                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         current_time = datetime.now(pytz.timezone(TIMEZONE))
